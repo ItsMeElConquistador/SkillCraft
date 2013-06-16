36ace7a50fe1ca00547f6e232da23e6e42da5113
ElConquistador <itsme.elconquistador@gmail.com>
2013-06-16 16:18:10 +0200

    Episode 6: Client Sided Skills

---
+++ src/elcon/mods/skillcraft/SkillClient.java
@@ -0,0 +1,32 @@
+package elcon.mods.skillcraft;
+
+import java.util.HashMap;
+
+public class SkillClient {
+
+	public static SkillPlayer player;
+	public static HashMap<String, SkillPlayer> players = new HashMap<String, SkillPlayer>();
+	
+	public static SkillPlayer getPlayer() {
+		return player;
+	}
+	
+	public static void setPlayer(SkillPlayer skillPlayer) {
+		player = skillPlayer;
+	}
+	
+	public static SkillPlayer getPlayer(String player) {
+		if(players.containsKey(player)) {
+			return players.get(player);
+		}
+		return null;
+	}
+	
+	public static void addPlayer(String player, SkillPlayer skillPlayer) {
+		players.put(player, skillPlayer);
+	}
+	
+	public static void removePlayer(String player) {
+		players.remove(player);
+	}
+}
