d9b08ee1a3bb003f3af9434742628b3fc2c60718
ElConquistador <itsme.elconquistador@gmail.com>
2013-07-23 17:30:32 +0200

    Updated to Minecraft 1.6.2

--- src/elcon/mods/skillcraft/SkillCraft.java
+++ src/elcon/mods/skillcraft/SkillCraft.java
@@ -2,10 +2,8 @@ package elcon.mods.skillcraft;
 
 import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.Mod.PostInit;
-import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
@@ -22,12 +20,12 @@ public class SkillCraft {
 	@SidedProxy(clientSide = "elcon.mods.skillcraft.ClientProxy", serverSide = "elcon.mods.skillcraft.CommonProxy")
 	public static CommonProxy proxy;
 	
-	@PreInit
+	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		PlayerSkill.calculateNeeded();
 	}
 	
-	@Init
+	@EventHandler
 	public void init(FMLInitializationEvent event) {
 		proxy.registerRenderInformation();
 		
@@ -36,7 +34,7 @@ public class SkillCraft {
 		MinecraftForge.EVENT_BUS.register(eventHandler);
 	}
 	
-	@PostInit
+	@EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
 		
 	}
